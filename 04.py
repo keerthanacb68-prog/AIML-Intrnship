3dce34698836f3261a35365ffa9a0a2f1dbde515
keerthanacb68-prog <keerthanacb68@gmail.com>
2026-02-05 15:13:42 +0530

    Add files via upload

---
+++ 04.py
@@ -0,0 +1,11 @@
+item_name = "Laptop"
+quantity = 2
+price = 499.99
+in_stock = True
+print("item:",item_name,"qty:",quantity,"price",price,"available:",in_stock)
+total_cost = quantity * price
+print("total cost:",total_cost)
+print("data type of item name:",type(item_name))
+print("data type of quantity:",type(quantity))
+print("data type of price:",type(price))
+print("data type of in stock:",type(in_stock))
